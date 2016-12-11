09df7389ec7b9c6250b0e44672c21e20d7289a7f
Tom Conroy <tom@thomasconroy.net>
2016-12-10 23:06:37 -0500

    sadsda

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -11,7 +11,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 		git init
 
 		# git remote add deploy "ssh://deploy@thomasconroy.net:/var/www/thomasconroy.net"
-		git remote add deploy "deploy@104.236.225.208:1337/var/www/thomasconroy.net"
+		#git remote add deploy "deploy@thomasconroy.net:1337/var/www/thomasconroy.net"
+		git remote add deploy "ssh://deploy@thomasconroy.net:1337/var/www/thomasconroy.net"
 		git config user.name "Travis CI"
 		git config user.email "tom+travisCI@thomasconroy.net"
 
