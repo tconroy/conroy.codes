be72f2c1aaf2e56d459a93876342f40c831b5810
Tom Conroy <tom@thomasconroy.net>
2016-12-10 22:17:29 -0500

    why not.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,20 +2,18 @@
 set -x
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-		# copy
-		mv --force dist ./public_html
-
 		# Initialize a new git repo in dist, and push it to our server.
-		# cd dist
-		# git init
+		cd dist
+		git init
 
 		# git remote add deploy "ssh://deploy@thomasconroy.net:/var/www/thomasconroy.net"
-		# git config --global user.name "Travis CI"
-		# git config --global user.email "tom+travisCI@thomasconroy.net"
+		git remote add deploy "ssh://$DEPLOY_USER:$DEPLOY_PASS@$DEPLOY_HOST:$DEPLOY_PATH"
+		git config --global user.name "Travis CI"
+		git config --global user.email "tom+travisCI@thomasconroy.net"
 
-		# git add .
-		# git commit -m "Deploy"
-		# git push --force deploy master
+		git add .
+		git commit -m "Deploy"
+		git push --force deploy master
 else
 	echo "Not deploying, since this branch isn't master."
 fi
