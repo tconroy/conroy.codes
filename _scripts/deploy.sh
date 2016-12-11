76d74d8e0c64c28ca9d3d799ca6d2311c50a90b1
Tom Conroy <tom@thomasconroy.net>
2016-12-10 21:06:46 -0500

    tweak8

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 set -x
-echo $(whoami);
-
-ADDRESSES=('thomasconroy.net:1337');
-
-for address in $ADDRESSES; do
-  ssh-keygen -F $address 2>/dev/null 1>/dev/null
-  if [ $? -eq 0 ]; then
-    echo “$address is already known”
-    continue
-   fi
-   ssh-keyscan -t rsa -T 10 $address >> ~/.ssh/known_hosts
-done
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
 		# Initialize a new git repo in dist, and push it to our server.
 		cd dist
 		git init
 
-		git remote add deploy "ssh://deploy@thomasconroy.net:1337/var/www/thomasconroy.net/.git"
+		# git remote add deploy "ssh://deploy@thomasconroy.net:1337/var/www/thomasconroy.net/.git"
+		git remote add deploy "git://deploy@thomasconroy.net:1337/var/www/thomasconroy.net"
 		git config --global user.name "Travis CI"
 		git config --global user.email "tom+travisCI@thomasconroy.net"
 
