44c9b4bca2e720c5d987da08b40dbefbf1dc86d9
Tom Conroy <tom@thomasconroy.net>
2016-12-10 22:58:43 -0500

    dsfsdf

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,13 +2,14 @@
 set -x
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-		# Initialize a new git repo in dist, and push it to our server.
+		# Initialize a new git repo in dist, and push it to our deploy repo on the webserver.
 		cd dist
 		git init
 
-		git remote add deploy "ssh://deploy@thomasconroy.net:/var/www/thomasconroy.net"
-		git config --global user.name "Travis CI"
-		git config --global user.email "tom+travisCI@thomasconroy.net"
+		# git remote add deploy "ssh://deploy@thomasconroy.net:/var/www/thomasconroy.net"
+		git remote add deploy "deploy@104.236.225.208:1337/var/www/thomasconroy.net"
+		git config user.name "Travis CI"
+		git config user.email "tom+travisCI@thomasconroy.net"
 
 		git add .
 		git commit -m "Deploy"
