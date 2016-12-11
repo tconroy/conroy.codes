033829465bf1edce34a909b315b30426c10fb305
Tom Conroy <tom@thomasconroy.net>
2016-12-10 19:50:16 -0500

    tweaks2

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,6 @@
-# #!/bin/bash
-# set -x
+#!/bin/bash
+set -x
 
+npm run test
 # timeout 30s bundle exec htmlproof _site --only-4xx --external_only --check-html --check-favicon
+# timeout 60s npm run test
