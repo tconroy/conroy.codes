f043fc3368690c88331968a28036c39595023db5
Tom Conroy <tom@thomasconroy.net>
2016-12-10 19:05:37 -0500

    this too

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,4 +3,4 @@ set -x
 # Build the site?
 
 # Compress assets with Zopfli
-zopfli/zopfli --i1000 dist/**/*.html dist/*.html  dist/**/*.css dist/*.css dist/**/*.js dist/*.js dist/**/*.xml dist/*.xml
+# zopfli/zopfli --i1000 dist/**/*.html dist/*.html  dist/**/*.css dist/*.css dist/**/*.js dist/*.js dist/**/*.xml dist/*.xml
