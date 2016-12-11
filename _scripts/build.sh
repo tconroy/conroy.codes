a2f55e9ca883ebb429ac719c94c537fbf03f74f9
Tom Conroy <tom@thomasconroy.net>
2016-12-10 19:18:42 -0500

    tweaks

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
-# Build the site?
+# Build the site
+npm run build
 
 # Compress assets with Zopfli
 # zopfli/zopfli --i1000 dist/**/*.html dist/*.html  dist/**/*.css dist/*.css dist/**/*.js dist/*.js dist/**/*.xml dist/*.xml
