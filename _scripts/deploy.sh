879d26489c35e3c3f7af4148c1e2f8c7d257cff4
Tom Conroy <tom@thomasconroy.net>
2016-12-10 20:28:25 -0500

    tweaks4

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 		cd dist
 		git init
 
-		git remote add deploy "ssh://deploy@thomasconroy.net:/var/www/thomasconroy.net"
+		git remote add deploy "ssh://deploy@thomasconroy.net:1337/var/www/thomasconroy.net/.git"
 		git config --global user.name "Travis CI"
 		git config --global user.email "tom+travisCI@thomasconroy.net"
 
