919ae3eb2ec63296c8e90a7764a644db97d50f44
Tom Conroy <tom@thomasconroy.net>
2020-11-24 13:48:15 -0500

    testing (8)

--- src/blog/__tests__/BlogGallery.test.tsx
+++ src/blog/__tests__/BlogGallery.test.tsx
@@ -1,17 +1,11 @@
-import {
-  render,
-} from '@testing-library/react';
-import {
-  BlogGallery
-} from '../BlogGallery';
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { BlogGallery } from '../BlogGallery';
 
 describe('BlogGallery', () => {
   it('should render the expected results', () => {
-    expect(render(
-      <BlogGallery
-        posts={[]}
-        pagination={{}}
-      />
-    ).container).toMatchSnapshot();
-  })
+    expect(render(<BlogGallery posts={[]} pagination={{}} />).container).toMatchSnapshot();
+  });
 });
