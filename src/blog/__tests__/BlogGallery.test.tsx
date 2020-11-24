4723d4face033640336c345121b1fb3d86265db5
Tom Conroy <tconroy@users.noreply.github.com>
2020-11-24 13:51:19 -0500

    Tconroy/feat/linting dev workflow (#2)
    Update workflows

---
+++ src/blog/__tests__/BlogGallery.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { BlogGallery } from '../BlogGallery';
+
+describe('BlogGallery', () => {
+  it('should render the expected results', () => {
+    expect(render(<BlogGallery posts={[]} pagination={{}} />).container).toMatchSnapshot();
+  });
+});
